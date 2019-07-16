1fed37356e21c9e445e7948b3b6b541efaefe0b9
Lucas Dias <lucas.dias@magically.com.br>
2019-07-16 12:24:31 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		loadChildren: './content/pages/pages.module#PagesModule'
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/content/pages/pages-routing.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { PagesModule } from './pages.module';
+
+const routes: Routes = [
+	{
+		path: '',
+		component: PagesComponent,
+		//canActivate: [NgxPermissionsGuard],
+		data: {
+			permissions: {
+				//Niveis
+				only: ['1', '2', '3', '4', '5', '6'],
+			 	except: ['GUEST'],
+				redirectTo: '/login'
+			}
+		},
+		children: [
+			{
+				path: 'hospitais',
+				loadChildren: './hospitais/hospitais.module#HospitaisModule'
+			}
+
+		]
+	},
+	{
+		path: 'login',
+		//canActivate: [NgxPermissionsGuard],
+		loadChildren: './auth/auth.module#AuthModule',
+		data: {
+			permissions: {
+				except: 'ADMIN'
+			}
+		},
+	},
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+
+  ],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
